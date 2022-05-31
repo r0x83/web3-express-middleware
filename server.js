e280c7f876ad2fa1c7656ef0da258f1b094316bf
imroshin <imroshin@gmail.com>
2022-05-31 22:06:07 +0530

    web3api

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const {web3api, configureWeb3} = require("./middleware");
+configureWeb3('7bb88087b3164f9985ad54c1cf1c693bacf2e99b3271aa1ab1e30f97a9c02bdb')
+
+app.use(web3api); // middleware for api
 
 app.get('/', (req, res) => {
   console.log("hi")

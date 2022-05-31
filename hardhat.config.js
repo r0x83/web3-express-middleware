80cf5cf2fdde00a0dba48fb9d7d5f119579f1d53
imroshin <imroshin@gmail.com>
2022-05-31 20:59:40 +0530

    confid

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
+ * 
  */
+ require('@nomiclabs/hardhat-ethers');
+//  require("@nomiclabs/hardhat-etherscan");
+
+ const { key, url,etherscanApiKey } = require('./secrets.json');
+
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.4",
+  networks: {
+           mainnet: {
+           url: url,
+           accounts: [key],
+        }
+      }
 };
